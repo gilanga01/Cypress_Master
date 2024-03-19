f345eb448e30e61b2276cf6aa96dfe6ff9aa4bf1
Gilbert Langat <Gilbert.Langat@dentsu.com>
2024-03-19 14:32:50 +0300

    Removed some files

--- cypress/e2e/mpesamenu.cy.js
+++
@@ -1,8 +0,0 @@
-describe('M-PESA Menu Restructure', () => {
-  it('passes', () => {
-    cy.visit('https://staging.isobarkenya.com/corporate/')
-    cy.get('.item-2013 > a').click()
-    cy.contains('Learn More').click().go('back')
-
-  })
-})
